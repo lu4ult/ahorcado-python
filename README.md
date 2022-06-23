a51af1a8d3ead99623b38a6e4aee17cb92616231
Lautaro <lu4ult@gmail.com>
2022-06-23 16:17:18 -0300

    se modifica readme y comentarios en el programa

--- README.md
+++ README.md
@@ -1,13 +1,20 @@
 # ahorcado-python
 Estas pocas líneas de python hacen un juego del clásico "ahorcado", lo interesante es que la palabra a adivinar se obtiene de la siguiente API:
+
 <a href="https://palabras-aleatorias-public-api.herokuapp.com/random" target="__blank">palabras-aleatorias-public-api.herokuapp.com/random</a>
 
-Si en lugar de ingresar una letra ingresamos la palabra 'pista' nos muestra la descripción asociada a la palabra.
+Si en lugar de ingresar una letra ingresamos la palabra <strong>'pista'</strong> nos muestra la descripción asociada a la palabra.
 
-<a href="https://imgur.com/TCmm0B0"><img src="https://i.imgur.com/TCmm0B0.png" title="source: imgur.com" /></a>
+Nota: intencionalmente no se hacen validación de entrada del usuario, ya que este programa es una breve demostración, una práctica y por diversión.
+Las vocales con y sin tilde se consideran caracteres distintos (a vs á)
 
-<a href="https://imgur.com/6jxNIZQ"><img src="https://i.imgur.com/6jxNIZQ.png" title="source: imgur.com" /></a>
 
-<a href="https://imgur.com/AVjPBQ9"><img src="https://i.imgur.com/AVjPBQ9.png" title="source: imgur.com" /></a>
+<a href="https://imgur.com/LjDZR9C"><img src="https://i.imgur.com/LjDZR9C.png"></a>
 
-<a href="https://imgur.com/flepTdA"><img src="https://i.imgur.com/flepTdA.png" title="source: imgur.com" /></a>
+
+<a href="https://imgur.com/TCmm0B0"><img src="https://i.imgur.com/TCmm0B0.png" title="source: imgur.com"/></a>
+<a href="https://imgur.com/6jxNIZQ"><img src="https://i.imgur.com/6jxNIZQ.png" title="source: imgur.com"/></a>
+
+<a href="https://imgur.com/AVjPBQ9"><img src="https://i.imgur.com/AVjPBQ9.png" title="source: imgur.com"/></a>
+
+<a href="https://imgur.com/flepTdA"><img src="https://i.imgur.com/flepTdA.png" title="source: imgur.com"/></a>
--- ahorcado.py
+++ ahorcado.py
@@ -6,33 +6,36 @@ respuesta = requests.get(url)
 palabra = respuesta.json()['body']['Word']
 definicion = respuesta.json()['body']['DefinitionMD']
 
-definicion = definicion.replace(palabra,'*' * len(palabra))  #aveces la definición de la palabra incluye la palabra en sí, por eso la reemplazamos por asteriscos para evitar verla
-definicion = definicion[:100]                                  #aveces la descripción trae un montón de otra información como sinónimos o códigos HTML, asi que limitamos la respuesta
-    
+#Aveces la definición de la palabra incluye la palabra en sí, por eso la reemplazamos por asteriscos para evitar verla accidentalmente.
+definicion = definicion.replace(palabra,'*' * len(palabra))
+#Aveces la descripción trae un montón de otra información, como sinónimos o códigos HTML, asi que limitamos el largo de la respuesta.
+definicion = definicion[:100]
+
 lista_letras = []
-vidas = 3
+vidas = 10
 
 print(f"Juego del ahorcado.\nPalabras clave:\n\t*rendirse\n\t*salir\n\t*pista\n")
 while(vidas):
-    print(f"\nVidas restantes: {vidas}\nLetras usadas:")
+    print(f"Vidas restantes: {vidas}\n")
+    print("Letras usadas:\n")
     for i in lista_letras:                              #Primero publicamos las letras que hemos ingreado, hayan sido acertadas o perdedoras
             print(i, end="-")
     print("\n")
 
     letras_acertadas = 0
-    for i in palabra:                                   #Recorremos cada letra dentro de la palabra random, 
+    for i in palabra:                                   #Recorremos cada letra dentro de la palabra random,
         if i in lista_letras:                           #si esa letra ya se seleccionó (se encuentra en la lista de letras) la pubicamos, sino publica un "_"
             letras_acertadas += 1
             print(i, end=" ")
         else:
             print("_", end=" ")
     print("\n\n")
-    
+
     if letras_acertadas == len(palabra):
         print("\n\n\t\tGANASTE!!")
         input("Presione cualquier tecla para salir")
         sys.exit()
-            
+
     letra = input("Ingrese letra: ").lower()
     os.system('cls')                                    #Para borrar la pantalla en DOS
     if letra == "pista":
@@ -41,12 +44,12 @@ while(vidas):
         break
     if letra == "rendirse":
         vidas = 0                                       #Colocamos las vidas en 0 ya que mas abajo se imprime el mensaje de "Perdiste"
-        
+
     if not letra in lista_letras and len(letra) == 1:   #Agregamos la letra a la lista de letras ingresadas y si no está en la palabra random descontamos una vida  
         lista_letras.append(letra)
-        lista_letras.sort()        
+        lista_letras.sort()
         if not letra in palabra:
             vidas -= 1
     if not vidas:
         print(f"PERDISTE!\nLa palabra era {palabra}")
-        input("Presione cualquier tecla para salir")                  
+        input("Presione cualquier tecla para salir")
